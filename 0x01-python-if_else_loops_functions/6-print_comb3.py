682a5e0a7cd1ecbab7d98fb04d47db02844a9ce9
kazibweyassin <kazibweusama@gmail.com>
2023-06-07 07:03:49 -0700

    loop

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-for i in range(1, 10):
-    for j in range(0, 10):
-        print(i*10+j)
+for i in range(0,9):
+    for j in range(i +1, 10):
+        if i !=8:
+            print(f"{i}{j}", end=', ')
+        else:
+            print(f"{i}{j}")
 
