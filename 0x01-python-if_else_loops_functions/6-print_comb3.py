a1381fa2f277012ec7452f462a211be04036a116
kazibweyassin <kazibweusama@gmail.com>
2023-06-07 07:10:48 -0700

    loop

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,5 +5,4 @@ for i in range(0, 9):
         if i != 8:
             print("{:d}{:d}".format(i, j), end=', ')
         else:
-            print("{:d}{:d}".format(i, j)
-
+            print("{:d}{:d}".format(i, j))
