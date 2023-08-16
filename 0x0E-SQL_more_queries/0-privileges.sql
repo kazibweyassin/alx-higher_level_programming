86c9adc5018985318e090886d0f5a72c734b8ef0
kazibweyassin <kazibweusama@gmail.com>
2023-08-16 05:41:21 -0700

    done

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
--- lits all privilages
----- Show grants for one user
+-- Show privileges
+-- Show grants for one user
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Show grants for other user
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
