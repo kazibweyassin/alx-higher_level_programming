e937fa2adf50e6cf77769a0e00f2115ae608c950
kazibweyassin <kazibweusama@gmail.com>
2023-08-15 23:14:53 -0700

    link

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,4 @@
---ists all records of the table second_table of the database
---Don’t list rows without a name value
-SELECT score FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+-- no name order
+SELECT score, name FROM second_table
+WHERE name <> ''
+GROUP BY score DESC, name DESC;
