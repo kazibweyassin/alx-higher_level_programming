bd45ffd45a1ab5691ae68ee7e2a5fd0b616f1444
kazibweyassin <kazibweusama@gmail.com>
2023-08-16 06:04:27 -0700

    new

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- Create user
--- Create user_0d_1 with password user_0d_1_pwd and all priveleges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- Set all privileges
+-- creates user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
