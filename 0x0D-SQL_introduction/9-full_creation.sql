0515d738f996bf76cef5aad118b514e72843c773
kazibweyassin <kazibweusama@gmail.com>
2023-08-15 22:50:28 -0700

    creattion

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,10 @@
---Creates a table 
-CREATE TABLE IF NOT EXISTS second_table(id INT PRIMARY KEY, name VARCHAR(256), score INT);
---Insert into table the values
-INSERT INTO second_table(id , name, score) VALUES(1, John, 10), (2, Alex, 3), (3, Bob, 14),(4, George, 8)
+-- creates and populates table
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT,
+`name` VARCHAR(256),
+`score` INT);
+INSERT INTO `second_table` (`id`,`name`, `score`)
+VALUES
+(1,"John", 10),
+(2,"Alex", 3),
+(3,"Bob", 14),
+(4,"George", 8);
