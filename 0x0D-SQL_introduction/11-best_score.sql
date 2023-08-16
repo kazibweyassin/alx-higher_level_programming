392fc51d9259554e2058951e8f9de5892946a7d9
kazibweyassin <kazibweusama@gmail.com>
2023-08-15 22:55:38 -0700

    best

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,5 @@
 --Top score 
 --List records starting with the top score only for  > = 10
-SELECT score, name FROM second_table WHERE score>=10 ORDER BY score  DESC
+SELECT score, name FROM second_table 
+WHERE score >= 10
+ORDER BY score  DESC
