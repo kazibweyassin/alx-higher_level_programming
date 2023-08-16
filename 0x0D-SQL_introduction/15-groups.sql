731b2106454a2b2aef7d51eb33026c80ba40d32a
kazibweyassin <kazibweusama@gmail.com>
2023-08-15 23:04:42 -0700

    group

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,4 @@
 -- List the number of records with the same score in the table
-SELECT score, COUNT(score) AS number FROM second_table
+SELECT score, COUNT(score) AS number 
+FROM second_table
 GROUP BY score DESC;
