8419021bf7647caa3d9b903873ecfd80e9e6b850
kazibweyassin <kazibweusama@gmail.com>
2023-09-29 22:19:59 -0700

    header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module uses the Universal Resource Library to make a request"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+
+    try:
+        # Send an HTTP GET request to the specified URL
+        with urllib.request.urlopen(url) as resp:
+            # Check if the 'X-Request-Id' header is present in the response
+            if 'X-Request-Id' in resp.headers:
+                x_request_id = resp.headers['X-Request-Id']
+                print("X-Request-Id:", x_request_id)
+            else:
+                print("X-Request-Id header not found in the response.")
+    except urllib.error.URLError as e:
+        print("Error:", e)
+    except Exception as e:
+        print("An error occurred:", e)
