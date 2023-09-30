9e0ee960736e1e8da50d3c18642a0ba91a222ab0
kazibweyassin <kazibweusama@gmail.com>
2023-09-29 22:09:28 -0700

    status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This modudile  uses the Universal resource library to make a request"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        html = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(str(html, 'UTF-8')))
