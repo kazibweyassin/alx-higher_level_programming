0f0c411ffdad2643090de5800774ad215b7e52f7
kazibweyassin <kazibweusama@gmail.com>
2023-11-24 12:00:06 +0000

    city

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""Start link class to table in database"""
+# Lists all City objects from the database hbtn_0e_14_usa.
+# Usage: ./14-model_city_fetch_by_state.py <mysql username> /
+#                                          <mysql password> /
+#                                          <database name>
 import sys
-from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 from model_city import City
 
-from sqlalchemy import (create_engine)
-
 if __name__ == "__main__":
-    x = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                                                    sys.argv[2],
-                                                    sys.argv[3])
-    engine = create_engine(x, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    from sqlalchemy.orm import sessionmaker
-
-    Session = sessionmaker()
-    Session.configure(bind=engine)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
-    q = session.query(City).order_by(City.id)
-    for row in q.all():
-        q2 = session.query(State).filter(State.id == row.state_id)
-        row2 = q2.first()
-        if row2:
-            print(str(row2.name) + ": (" + str(row.id) + ") " + str(row.name))
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
