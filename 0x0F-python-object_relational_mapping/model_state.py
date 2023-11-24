22b985120cffe092a75dd1d25c65b2bc891428f2
kazibweyassin <kazibweusama@gmail.com>
2023-11-24 11:10:28 +0000

    state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,17 +2,15 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import sessionmaker
+
 Base = declarative_base():
 
 class State(Base):
-    """State class"""
-    __table__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
-    name = Column(String(128), nullable=False)
-    
-    #create engine and bind it to MQSL server
-engine = create_engine('mysql+mysqldb://username:password@localhost:3306/database_name')
-
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+    """Represent a state for Mysql database.
+    __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = column(String(128), nullable=False)
