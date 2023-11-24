aaadcfa88736302ff9df3ddfa50fa9370389c980
kazibweyassin <kazibweusama@gmail.com>
2023-11-24 12:04:29 +0000

    city

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-# Lists all City objects from the database hbtn_0e_14_usa.
-# Usage: ./14-model_city_fetch_by_state.py <mysql username> /
-#                                          <mysql password> /
-#                                          <database name>
-import sys
-from sqlalchemy import create_engine
+
+""" Module to query the database for all states"""
+
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sqlalchemy import create_engine
+from model_state import Base, State
 from model_city import City
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
+
+def list_cities():
+    """ Function that lists all the states"""
     session = Session()
+    query = session.query(City, State.name).join(State).all()
+    for city, state_name in query:
+        print(f"{state_name}: ({city.id}) {city.name}")
+    session.close()
 
-    for city, state in session.query(City, State) \
-                              .filter(City.state_id == State.id) \
-                              .order_by(City.id):
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
+if __name__ == "__main__":
+    import sys
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    host = "localhost"
+    path = f'mysql+mysqldb://{usr}:{pwd}@{host}/{db}'
+    engine = create_engine(path, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    list_cities()
